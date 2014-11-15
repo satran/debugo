fbcfb9f618f75b469c491e35a90bf3b7ac2e06c3
Satyajit Ranjeev <satyajit.ranjeev@gmail.com>
2014-11-15 23:41:54 +0530

    Moving the static files to their own package.

---
+++ cmd/debugo/static/css.go
@@ -0,0 +1,8 @@
+package static
+
+var Css = `
+body,p,div,span,li,ul{
+	margin:0;
+	padding:0;
+}
+`
---
+++ cmd/debugo/static/index.go
@@ -0,0 +1,16 @@
+package static
+
+var Index = `
+<!doctype html>
+<head>
+	<link rel="stylesheet" href="/s/base.css" />
+	<title>Debugo</title>
+</head>
+
+<body>
+	<script type="text/template" id="tmpl-"></script>
+	<script src="/s/underscore.js"></script>
+	<script src="/s/backbone.js"></script>
+	<script src="/s/base.js"></script>
+</body>
+`
